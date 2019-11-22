175dc966798a6ef4ef8fc7a79c226274fa2ad109
Naqib Hossain <naqib.hossain@lifetouch.com>
2019-11-22 14:24:13 -0600

    added all file till date

---
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -0,0 +1,38 @@
+package com.userfront.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.userfront.domain.User;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "redirect:/index";
+	}
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signup(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		
+		return "signup";
+	}
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public void signupPost(@ModelAttribute("user") User user, Model model) {
+		
+		
+		//return "redirect://";
+	}
+}
